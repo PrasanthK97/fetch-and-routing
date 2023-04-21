fe640d7025cd79ad05f521bca5c6c4ad67ad86aa
PrasanthK97 <prasantheits@gmail.com>
2023-04-21 14:10:05 +0000

    commit fetch message

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,41 @@
+// Write your JS code here
+import {Component} from 'react'
+
+import BlogItem from '../BlogItem'
+
+class BlogList extends Component {
+  state = {blogData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getDataFromUrl()
+  }
+
+  getDataFromUrl = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const formattedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogData: formattedData})
+    console.log(data)
+  }
+
+  render() {
+    const {blogData} = this.state
+
+    return (
+      <div testid="loader">
+        {blogData.map(item => (
+          <BlogItem blogData={item} key={item.id} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default BlogList
